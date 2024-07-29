45f0335cec12e47f7d094070340e9b1165449007
Saurabh00021 <116018058+Saurabh00021@users.noreply.github.com>
2024-07-29 19:52:08 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String [] args){
-        System.out.println("hii world");
+        System.out.println("hello world");
         System.out.println("This is my 1st project");
     }
 }
